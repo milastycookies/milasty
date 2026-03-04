2f489f5e2034627bdd988615c4c33b3a945e3739
milastycookies <milasty.cookies@gmail.com>
2026-03-04 13:41:55 +0530

    Update cart-ui.js

--- js/cart-ui.js
+++ js/cart-ui.js
@@ -32,3 +32,28 @@ Subtotal: ₹${total}<br>
 Delivery: ₹${delivery}<br>
 <b>Total: ₹${finalTotal}</b>
 `;
+
+
+
+
+
+function sendOrder(){
+
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+let address = document.getElementById("address").value;
+
+let cart = getCart();
+
+let message = "Hello MILASTY,%0A%0AI would like to place an order.%0A%0A";
+
+cart.forEach(item=>{
+message += item.name + " x " + item.qty + "%0A";
+});
+
+message += "%0AName: " + name;
+message += "%0APhone: " + phone;
+message += "%0AAddress: " + address;
+
+window.open("https://wa.me/918927142056?text="+message);
+}
