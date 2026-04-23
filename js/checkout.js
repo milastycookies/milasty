8c60464f3f773112d91352e50a766b520f9b5298
milastycookies <milasty.cookies@gmail.com>
2026-04-23 16:37:12 +0530

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -77,9 +77,6 @@ async function startCheckout(orderToken){
     return;
   }
 
-  window.otpVerified = true;
-  console.log("OTP VERIFIED:", window.otpVerified);
-
   const cart = getCart();
 
   if (!cart || cart.length === 0) {
